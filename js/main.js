b9c1bc2a02e95fe70b353d344acfe75e05aff428
tanjonarako <tanjona.rakotoarisoa@applidget.com>
2016-07-06 17:33:02 +0200

    modify css and js

--- js/main.js
+++ js/main.js
@@ -3,37 +3,37 @@ function handleColor(bloc) {
     $(".image_fond").removeClass("imagefond_green")
                     .removeClass("imagefond_blue")
                     .removeClass("imagefond_pink");
-    $("body").removeClass("green")
+    $("body, .progress-count").removeClass("green")
              .removeClass("blue")
              .removeClass("pink");
     $(".image_fond").addClass("imagefond_red");
-    $("body").addClass("red");
+    $("body, .progress-count").addClass("red");
   } else if (bloc.hasClass("bloc-blue")) {
     $(".image_fond").removeClass("imagefond_green")
                     .removeClass("imagefond_red")
                     .removeClass("imagefond_pink");
-    $("body").removeClass("green")
+    $("body, .progress-count").removeClass("green")
              .removeClass("red")
              .removeClass("pink");
-    $("body").addClass("blue");
+    $("body, .progress-count").addClass("blue");
     $(".image_fond").addClass("imagefond_blue");
   } else if (bloc.hasClass("bloc-green")) {
     $(".image_fond").removeClass("imagefond_blue")
                     .removeClass("imagefond_red")
                     .removeClass("imagefond_pink");
-    $("body").removeClass("blue")
+    $("body, .progress-count").removeClass("blue")
              .removeClass("red")
              .removeClass("pink");
-    $("body").addClass("green");
+    $("body, .progress-count").addClass("green");
     $(".image_fond").addClass("imagefond_green");
   } else if (bloc.hasClass("bloc-pink")) {
     $(".image_fond").removeClass("imagefond_blue")
                     .removeClass("imagefond_red")
                     .removeClass("imagefond_green");
-    $("body").removeClass("blue")
+    $("body, .progress-count").removeClass("blue")
              .removeClass("red")
              .removeClass("green");
-    $("body").addClass("pink");
+    $("body, .progress-count").addClass("pink");
     $(".image_fond").addClass("imagefond_pink");
   }
 }
